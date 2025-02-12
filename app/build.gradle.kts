b529b4992adb7e97ce1bd9054187d60f20f9c922
d.n_ry <danlryzhov@gmail.com>
2025-02-12 16:06:58 +0300

    Adding dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -42,6 +43,24 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    implementation (libs.dagger)
+    ksp (libs.dagger.android.processor)
+    ksp (libs.dagger.compiler)
+
+    implementation(libs.retrofit)
+    implementation(libs.gson)
+    implementation (libs.glide)
+    ksp (libs.compiler)
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+    implementation (libs.androidx.lifecycle.livedata.ktx)
+
+    implementation (libs.kotlinx.coroutines.core)
+    implementation (libs.kotlinx.coroutines.android)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
